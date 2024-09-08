87c92364dfdf524e81458ef5253df47babd747a7
arnakr <aks2@hi.is>
2024-09-08 18:45:24 +0000

    Arna

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    // This method handles HTTP GET requests to "/"
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Home Page Allir!";
+    }
+}
