2ec3d98fe2ed6b26dbe1c49ceb56458c5a2f558f
Ilaria20 <irc3@hi.is>
2024-09-27 10:10:49 +0200

    SearchController

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,13 +2,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     // This method handles HTTP GET requests to "/"
-    @GetMapping("/")
+    @RequestMapping ("/")
     public String HomeController() {
 
         return "home";
