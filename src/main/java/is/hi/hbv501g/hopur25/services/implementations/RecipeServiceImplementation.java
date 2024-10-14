f50bd4e6b371a6c50105bb56e6c3265a8a4b3490
arnakr <aks2@hi.is>
2024-10-14 08:23:22 +0000

    recipe lagfæringar fyrir database

--- src/main/java/is/hi/hbv501g/hopur25/services/implementations/RecipeServiceImplementation.java
+++ src/main/java/is/hi/hbv501g/hopur25/services/implementations/RecipeServiceImplementation.java
@@ -17,42 +17,49 @@ public class RecipeServiceImplementation implements RecipeService {
     //private int id_counter = 0;
 
     @Autowired
-    public RecipeServiceImplementation() {
-        this.recipeRepository = RecipeRepository;
+    public RecipeServiceImplementation(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
        // recipeRepository.add(new Recipe("title 1", new ArrayList<>(), 1 , "description 1"));
         //recipeRepository.add(new Recipe("title 2", new ArrayList<>(), 2 , "description 2"));
         //recipeRepository.add(new Recipe("title 3", new ArrayList<>(), 3 , "description 3"));
         //for (Recipe r: recipeRepository) {
           //  r.setRecipeID(id_counter);
             //id_counter++;
-        }
     }
 
     @Override
     public List<Recipe> findAll() {
-        return recipeRepository;
+        return recipeRepository.findAll();
     }
 
     @Override
-    public Recipe findByID(long ID) {
+    /*public Recipe findByID(long ID) {
         for (Recipe r: recipeRepository) {
             if (r.getRecipeID() == ID) {
                 return r;
             }
         }
-        return null;
+        return null;*/
+    public Recipe findByID(long ID) {
+        return recipeRepository.findById(ID);
+    }
+
+    @Override
+    public Recipe findByTitle(String title) {
+        return recipeRepository.findByTitle(title).getFirst(); //skoða hérna hvað við viljum sækja
     }
 
     @Override
     public Recipe addRecipe(Recipe recipe) {
-        recipe.setRecipeID(id_counter);
+        /*recipe.setRecipeID(id_counter);
         id_counter++;
         recipeRepository.add(recipe);
-        return recipe;
+        return recipe;*/
+        return recipeRepository.save(recipe);
     }
 
     @Override
-    public void deleteRecipe(Recipe recipe) {
-        recipeRepository.remove(recipe);
+    public void delete(Recipe recipe) {
+        recipeRepository.delete(recipe);
     }
 }
