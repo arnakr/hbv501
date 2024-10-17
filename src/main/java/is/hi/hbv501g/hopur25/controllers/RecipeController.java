b6be0e3ac5a4daab41b944831b86c81af7a9690b
arnakr <aks2@hi.is>
2024-10-17 19:58:49 +0000

    User favorites

--- src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
+++ src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
@@ -1,19 +1,20 @@
 package is.hi.hbv501g.hopur25.controllers;
 
 import is.hi.hbv501g.hopur25.persistence.entities.Recipe;
+import is.hi.hbv501g.hopur25.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import is.hi.hbv501g.hopur25.services.RecipeService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private UserService userService;
 
     @Autowired
     public RecipeController(RecipeService recipeService) {
@@ -24,9 +25,24 @@ public class RecipeController {
     public String getRecipeById(@PathVariable("id") Long id, Model model) {
         Recipe recipe = recipeService.findRecipeById(id);
         if (recipe == null) {
-            return "error"; // Handle error page if recipe not found
+            return "error";
         }
         model.addAttribute("recipe", recipe);
-        return "recipe"; // This is the name of the Thymeleaf template you want to display
+        return "recipe";
+    }
+
+
+    @GetMapping("/user/{userId}/favorites")
+    public String getFavoriteRecipes(@PathVariable Long userId, Model model) {
+        model.addAttribute("favorites", userService.getUserFavorites(userId));
+        return "favorites";
+    }
+
+
+    @PostMapping("/user/{userId}/favorites/{recipeId}")
+    public String addFavoriteRecipe(@PathVariable Long userId, @PathVariable Long recipeId) {
+        Recipe recipe = recipeService.findRecipeById(recipeId);
+        userService.addFavoriteRecipe(userId, recipe);
+        return "redirect:/user/" + userId + "/favorites";
     }
 }
