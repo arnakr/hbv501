2ce3ebc7f03e8c3ed2b714d01fa1bce3cca01f7e
arnakr <aks2@hi.is>
2024-10-17 19:08:42 +0000

    recipe id

--- src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
+++ src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class RecipeController {
 
