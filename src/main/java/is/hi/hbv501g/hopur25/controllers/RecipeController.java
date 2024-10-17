c1f681e5cb07bd03250ac7e37c6c84e684faedf8
arnakr <aks2@hi.is>
2024-10-17 12:31:37 +0000

    Retrieve a single recipe

--- src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
+++ src/main/java/is/hi/hbv501g/hopur25/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package is.hi.hbv501g.hopur25.controllers;
 
+
 import is.hi.hbv501g.hopur25.persistence.entities.Recipe;
 import is.hi.hbv501g.hopur25.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,22 +23,59 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-   /** @GetMapping("/{id}")
-    public ResponseEntity<?> getRecipeById(@PathVariable("id") long id) {
+    @GetMapping("/recipe/{id}")
+    public String getRecipeById(@PathVariable("id") long id, Model model) {
         Optional<Recipe> recipe = recipeService.findById(id);
         if (recipe.isPresent()) {
-            return new ResponseEntity<>(recipe.get().toString(), HttpStatus.OK);
+            model.addAttribute("recipe", recipe.get());
+            return "recipe";
+        } else {
+            model.addAttribute("message", "Recipe not found");
+            return "errorPage";  // Optional error page template
         }
-        return new ResponseEntity<>("Uppskrift ekki til", HttpStatus.OK);
-    }*/
+    }
+}
+
+//import is.hi.hbv501g.hopur25.persistence.entities.Recipe;
+//import is.hi.hbv501g.hopur25.services.RecipeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+//import java.util.Optional;
+
+/*@Controller
+@RequestMapping("/recipe")
+public class RecipeController {
+
+    private RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+   /**
+    * @GetMapping("/{id}") public ResponseEntity<?> getRecipeById(@PathVariable("id") long id) {
+    * Optional<Recipe> recipe = recipeService.findById(id);
+    * if (recipe.isPresent()) {
+    * return new ResponseEntity<>(recipe.get().toString(), HttpStatus.OK);
+    * }
+    * return new ResponseEntity<>("Uppskrift ekki til", HttpStatus.OK);
+    * }
+
    @GetMapping("/recipe/{id}")
-   public ResponseEntity<?> getRecipeById(@PathVariable("id") long id) {
+   public String getRecipeById(@PathVariable("id") long id) {
        Optional<Recipe> recipe = recipeService.findById(id);
        if (recipe.isPresent()) {
            return new ResponseEntity<>("Recipe ID: " + recipe.get().getRecipeId() + ", Title: " + recipe.get().getTitle(), HttpStatus.OK);
        } else {
-           return new ResponseEntity<>("Uppskrift ekki til", HttpStatus.NOT_FOUND);
+           new ResponseEntity<>("Uppskrift ekki til", HttpStatus.NOT_FOUND);
        }
    }
 }
-
+*/
