d32bc218c998798a5b23a339106d66129a67f250
arnakr <aks2@hi.is>
2024-11-15 15:24:30 +0000

    Bæta við umsögn sem einkunn frá 1-5

--- src/main/java/is/hi/hbv501g/hopur25/controllers/ReviewController.java
+++ src/main/java/is/hi/hbv501g/hopur25/controllers/ReviewController.java
@@ -1,12 +1,45 @@
 package is.hi.hbv501g.hopur25.controllers;
 
+import is.hi.hbv501g.hopur25.persistence.entities.Review;
+import is.hi.hbv501g.hopur25.persistence.entities.User;
+import is.hi.hbv501g.hopur25.services.RecipeService;
+import is.hi.hbv501g.hopur25.services.ReviewService;
+import is.hi.hbv501g.hopur25.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewController {
+    private final RecipeService recipeService;
+    private final UserService userService;
+    private final ReviewService reviewService;
 
-    public String ReviewController() {
+    @Autowired
+    public ReviewController(RecipeService recipeService, UserService userService, ReviewService reviewService) {
+        this.recipeService = recipeService;
+        this.userService = userService;
+        this.reviewService = reviewService;
+    }
 
-        return "";
+    @PostMapping("/recipe/{recipeId}/addReview")
+    public String addReview(@PathVariable Long recipeId, @RequestParam String comment, @RequestParam int rating, HttpSession session) {
+        User loggedInUser = (User) session.getAttribute("LoggedInUser");
+        if (loggedInUser == null) {
+            return "redirect:/login";
+        }
+
+        Review review = new Review();
+        review.setComment(comment);
+        review.setRecipe(recipeService.findRecipeById(recipeId));
+        review.setUser(loggedInUser);
+        review.setRating(rating);
+
+        reviewService.saveReview(review);
+
+        return "redirect:/recipe/" + recipeId;
     }
 }
